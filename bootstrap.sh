ad985ab7b6558e7a31cdbf0c13fef64269e3286e
Charlie Chiang <charlie_c_0129@outlook.com>
2022-05-14 00:36:11 +0800

    feat: make sure repo is in the correct location
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# cd into where this script lives
+SCRIPT_DIR="$(dirname "${BASH_SOURCE}")"
+cd "${SCRIPT_DIR}" || exit 1
+
+# Make sure SCRIPT_DIR is $HOME/dotfiles
+if [ "${PWD}" != "${HOME}/dotfiles" ]; then
+    echo "The whole dotfiles repo must be cloned to ${HOME}/dotfiles" 1>&2
+    echo "Currently in ${PWD}" 1>&2
+    exit 1
+fi
+
 FORCE=""
 
 # Parse arguments
@@ -24,8 +35,7 @@ function show_ln_fail_help() {
     exit 1
 }
 
-# cd into where this script lives
-cd "$(dirname "${BASH_SOURCE}")" || exit 1
+
 
 # link files
 ln -s ${FORCE} "${PWD}/.zshrc" ~/.zshrc || show_ln_fail_help
