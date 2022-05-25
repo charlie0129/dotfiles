cadd648da6bb9212b438697368f1d17dc4f40e17
Charlie Chiang <charlie_c_0129@outlook.com>
2022-05-26 00:16:16 +0800

    feat: cloc1 supports custom dirs
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- alias/common.sh
+++ alias/common.sh
@@ -7,7 +7,7 @@ alias htop='sudo htop'
 alias gcl1='git clone --depth=1'
 
 # cloc with common non-relevant dirs excluded
-alias cloc1="cloc . --exclude-dir=node_modules,.idea,.mvn,.git --exclude-ext=.log,.lock,.tsbuildinfo"
+alias cloc1="cloc --exclude-dir=node_modules,.idea,.mvn,.git --exclude-ext=.log,.lock,.tsbuildinfo"
 
 # Short for docker-compose
 alias comp="docker-compose"
