0dee3353399a2c58c42d24371bb5a91faa616a1e
Charlie Chiang <charlie_c_0129@outlook.com>
2022-06-24 02:32:35 +0800

    feat: add zstandard compression and decompression aliases
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- alias/common.sh
+++ alias/common.sh
@@ -9,6 +9,10 @@ alias gcl1='git clone --depth=1'
 # cloc with common non-relevant dirs excluded
 alias cloc1="cloc --exclude-dir=node_modules,.idea,.mvn,.git --exclude-ext=.log,.lock,.tsbuildinfo"
 
+# Zstandard related
+alias tzst="tar --preserve-permissions --use-compress-program zstd -cf"
+alias untzst="tar --preserve-permissions --use-compress-program zstd -xf"
+
 # Short for docker-compose
 alias comp="docker-compose"
 
