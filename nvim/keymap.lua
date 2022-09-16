494d0107269ca47e22a49b8d2d926c88015badf4
Charlie Chiang <charlie_c_0129@outlook.com>
2022-09-17 02:55:16 +0800

    fix neovim config keymap

--- nvim/keymap.lua
+++ nvim/keymap.lua
@@ -1,10 +1,21 @@
-local Remap = require("theprimeagen.keymap")
-local nnoremap = Remap.nnoremap
-local vnoremap = Remap.vnoremap
-local inoremap = Remap.inoremap
-local xnoremap = Remap.xnoremap
-local nmap = Remap.nmap
+local function bind(op, outer_opts)
+    outer_opts = outer_opts or {noremap = true}
+    return function(lhs, rhs, opts)
+        opts = vim.tbl_extend("force",
+            outer_opts,
+            opts or {}
+        )
+        vim.keymap.set(op, lhs, rhs, opts)
+    end
+end
+
+local nmap = bind("n", {noremap = false})
+local nnoremap = bind("n")
+local vnoremap = bind("v")
+local xnoremap = bind("x")
+local inoremap = bind("i")
 
 xnoremap("<leader>p", "\"_dP")
 
 inoremap("<C-c>", "<Esc>")
+
