52e7179b8bbfc5d47b64050a294cc6a229994c2f
Charlie Chiang <charlie_c_0129@outlook.com>
2023-03-09 16:18:24 +0800

    add descriptions to custom.sh

--- env/custom.sh
+++ env/custom.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# This file is not tracked by git. You can put env definitions that are
+# specific to this computer here, without being synced to upstream or other computers.
+
 # This list is inserted before PATH
 PATH_BEFORE=(
     # custom bin in this repo, i.e. bin/custom
