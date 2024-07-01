9f15f8ef8f3a2bfd707878d8154d1989e2344ec7
Charlie Chiang <charlie_c_0129@outlook.com>
2024-07-01 19:44:47 +0800

    nix shell
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- dep/sync-mirror.sh
+++ dep/sync-mirror.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+## WARNING: NOT FINISHED YET
+
 cd "${BASH_SOURCE%/*}" || exit
 
 source ../bin/util/logger.sh
@@ -12,11 +14,16 @@ for repo in *; do
         continue
     fi
     cd "$repo"
+    rm -rf $TMPDIR/$repo
+    origin=$(git remote get-url origin)
+    git clone --bare "$origin" $TMPDIR/$repo
     # Add mirror remote. Please make sure this repo exists on the mirror.
     if ! git remote get-url "$mirror_remote_name" >/dev/null 2>&1; then
         info "Adding remote $mirror_remote_name..."
         git remote add "$mirror_remote_name" "$mirror_user/$repo" >/dev/null
     fi
+    info "Fetching $repo..."
+    git fetch --unshallow >/dev/null
     info "Pushing $repo to $mirror_remote_name..."
     git push --force --all "$mirror_remote_name" >/dev/null
     cd ..
