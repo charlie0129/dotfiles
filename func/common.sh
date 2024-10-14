8a65490b17a8621312bb72a832a51e91859703e0
Charlie Chiang <charlie_c_0129@outlook.com>
2024-10-14 11:01:21 +0800

    add ls colors and funcs
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- func/common.sh
+++ func/common.sh
@@ -10,3 +10,35 @@ ksw() {
     local filename=$(basename $chosenconfig)
     echo -e "- export KUBECONFIG=$dir/\033[32;1m$filename\033[0m"
 }
+
+# mnemonic: [F]ind [P]ath
+fp() {
+    local loc=$(echo $PATH | sed -e $'s/:/\\\n/g' | eval "fzf ${FZF_DEFAULT_OPTS} --header='[find:path]'")
+
+    if [[ -d $loc ]]; then
+        echo "$(rg --files $loc | rev | cut -d"/" -f1 | rev)" | eval "fzf ${FZF_DEFAULT_OPTS} --header='[find:exe] => ${loc}' >/dev/null"
+        fp
+    fi
+}
+
+# mnemonic: [K]ill [P]rocess
+kp() {
+    local pid=$(ps -ef | sed 1d | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:process]'" | awk '{print $2}')
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+        kp
+    fi
+}
+
+# mnemonic: [K]ill [S]erver
+ks() {
+    local pid=$(lsof -Pwni | sed 1d | grep -e LISTEN -e '\*:' | eval "fzf ${FZF_DEFAULT_OPTS} -m --header='[kill:tcp]'" | awk '{print $2}')
+
+    if [ "x$pid" != "x" ]
+    then
+        echo $pid | xargs kill -${1:-9}
+        ks
+    fi
+}
