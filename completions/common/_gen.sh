5bef1cf7fa28a0c690b493fa478463ea4012b939
Charlie Chiang <charlie_c_0129@outlook.com>
2024-12-20 17:08:54 +0800

    update
    Signed-off-by: Charlie Chiang <charlie_c_0129@outlook.com>

--- completions/common/_gen.sh
+++ completions/common/_gen.sh
@@ -7,3 +7,4 @@ dufs --completions zsh >dufs.zsh
 k9s completion zsh >k9s.zsh
 cat $HOME/.bun/_bun >bun.zsh
 pm2 completion >pm2.zsh
+curl -L https://github.com/lukechilds/zsh-better-npm-completion/raw/refs/heads/master/zsh-better-npm-completion.plugin.zsh >npm.zsh
