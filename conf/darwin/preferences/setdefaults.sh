737641dd648612255b2457e8996c163fc3052abd
Charlie Chiang <charlie_c_0129@outlook.com>
2025-06-19 21:45:34 +0800

    safari developer menu

--- conf/darwin/preferences/setdefaults.sh
+++ conf/darwin/preferences/setdefaults.sh
@@ -606,6 +606,9 @@ defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
 # Enable the Develop menu and the Web Inspector in Safari
 defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari DeveloperMenuVisibility -bool true
+defaults write com.apple.Safari.SandboxBroker ShowDevelopMenu -bool true
+defaults write com.apple.Safari WebKitPreferences.developerExtrasEnabled -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
 
